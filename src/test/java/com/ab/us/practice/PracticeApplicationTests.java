7e451944d882d3a01e0d7d54b5bd375fd4ceaf18
DJ027217 <2312379127@qq.com>
2020-08-04 10:49:25 +0800

    git练习提交

---
+++ src/test/java/com/ab/us/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ab.us.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
